c14a503c4345d5afa56506feb6b83f8072072027
AlexEn8 <noreply>
2022-09-20 20:21:04 -0700

    Fixed Ex6

--- src/test/java/Ex6RedirectTest.java
+++ src/test/java/Ex6RedirectTest.java
@@ -5,27 +5,20 @@ import org.junit.jupiter.api.Test;
 public class Ex6RedirectTest {
 
     @Test
-    public void testProba(){
-        Boolean doRequest = true;
+    public void testEx6Redirect() {
+
         String requestUrl = "https://playground.learnqa.ru/api/long_redirect";
 
-        while (doRequest) {
-            Response response = RestAssured
-                    .given()
-                    .redirects()
-                    .follow(false)
-                    .when()
-                    .get(requestUrl)
-                    .andReturn();
-            String locationHeader = response.getHeader("Location");
-            if (locationHeader != null) {
-                requestUrl = locationHeader;
-            } else {
-                doRequest = false;
-            }
-            if (response.getStatusCode() == 200){
-                System.out.println(response.getHeader("X-Host"));
-            }
-        }
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .when()
+                .get(requestUrl)
+                .andReturn();
+
+        System.out.println(response.getHeader("X-Host"));
     }
 }
+
+
---
+++ src/test/java/Ex7LongRedirectTest.java
@@ -0,0 +1,33 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex7LongRedirectTest {
+
+    @Test
+    public void testEx7LongRedirect(){
+        Boolean doRequest = true;
+        String requestUrl = "https://playground.learnqa.ru/api/long_redirect";
+
+        while (doRequest) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(requestUrl)
+                    .andReturn();
+            String locationHeader = response.getHeader("Location");
+            if (locationHeader != null) {
+                requestUrl = locationHeader;
+            } else {
+                doRequest = false;
+            }
+            if (response.getStatusCode() == 200){
+                System.out.println(response.getHeader("X-Host"));
+            }
+        }
+    }
+}
+
+
